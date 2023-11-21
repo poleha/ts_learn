793580de3b4570612d9c88a55ef230abad6f5ff9
Alex Polekha <a.polekha@gmail.com>
2023-11-21 18:13:34 +0400

    first commit

---
+++ learn.ts
@@ -0,0 +1,42 @@
+//*****
+
+type GetSecondArg<T> = T extends (first: any, second: infer SecondArg, ...args: any[]) => any ? SecondArg : never
+
+type NumberFunc = (a: number, b: number, c:string) => number
+
+type Second = GetSecondArg<NumberFunc> // number
+
+//*****
+
+type Getter<T> = {
+    [Key in keyof T as `get${Capitalize<Key & string>}`]: () => T[Key]
+}
+
+type Person = {
+    1: number,
+    name: string
+}
+
+type GetPerson = Getter<Person>
+
+
+let person: Person = {
+    1:2,
+    name: 'Test'
+}
+
+let getPerson: GetPerson = {
+    //get1: () => person[1], // error because number & string is never
+    getName: () => person.name,
+}
+
+//*****
+
+type MyReturnType<T> = T extends (...args:any[]) => infer ReturnType ? ReturnType : never;
+
+type TestFunction = () => string | number;
+
+type TestFunctionReturnType = MyReturnType<TestFunction> // string | number
+
+
+
